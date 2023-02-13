2112c55b42d07c16a45af45856322fa20a37efa0
JoseBarreto <barretojantonio@gmail.com>
2023-02-12 22:06:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+Swal.fire({
+title: "Bienvenido",
+text: "Espero que disfrute mi web."
+});
+
+function alerta(){
+    alert ("Comenzará la descarga del archivo.");
+};
+
+function alertafoto(){
+    alert ("Imágen correspondiente a CV José Barreto.");
+};
+
